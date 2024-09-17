992078961555f24ef49a0cbb3811f61bcc77be3d
e28166 <e28166@FXH-0015840124>
2024-09-17 13:48:25 -0400

    s

---
+++ src/sandbox/Test.java
@@ -0,0 +1,28 @@
+package sandbox;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		int num = 113;
+		
+		sum = sum + (num % 10);
+		
+		System.out.println(sum);
+		
+		// change 113 to 11 ???
+		num = num / 10;
+		System.out.println(num);
+
+		// What will be displayed?
+		
+		int num2 = 1 / 10;
+		System.out.println(num2);
+		
+		
+		
+		
+	}
+
+}
