e5d4ff0252c974dd167bec1e9d4db618ba02cab5
e28166 <e28166@FXH-0015840124>
2024-11-04 14:07:41 -0500

    s

---
+++ src/lessons/FileOutput.java
@@ -0,0 +1,29 @@
+package lessons;
+import java.util.ArrayList;
+import java.io.*;
+
+
+public class FileOutput {
+
+	public static void main(String[] args) {
+
+		// variables
+		String[] food = {"milk", "bread", "butter", "cheese", "yogurt"};
+		
+		try {
+			
+			// create file
+			FileOutputStream fout = new FileOutputStream("C:\\Users\\e28166\\Desktop");
+			C:\Users\e28166\Desktop
+			
+			
+		}
+		
+		catch(Exception e)  {
+			
+			e.printStackTrace();
+		}
+		
+	}
+
+}
