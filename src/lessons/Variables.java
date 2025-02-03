0c3745b2325227dec89b83ed920f40dfcbc60b04
e28166 <e28166@FXH-0015840124>
2025-02-03 13:45:42 -0500

    ss

---
+++ src/lessons/Variables.java
@@ -0,0 +1,14 @@
+package lessons;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		// variables
+		int myInteger = 13;
+		double myDouble = 13.5;
+		boolean myBoolean = true;
+
+	}
+
+}
