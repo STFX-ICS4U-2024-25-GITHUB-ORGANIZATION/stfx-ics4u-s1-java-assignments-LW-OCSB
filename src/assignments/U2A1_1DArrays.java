57ed7ffb95dae9128f6cb6115c01a5b9f6b628c5
e28166 <e28166@FXH-0015840124>
2025-02-10 12:28:18 -0500

    s

---
+++ src/assignments/U2A1_1DArrays.java
@@ -0,0 +1,60 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class U2A1_1DArrays {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+	
+	
+	// DESCRIPTION - Displays main menu, returns user choice
+	// PARAMETERS - NA
+	// RETURN TYPE - int
+	public static int displayMenu()  {
+		
+
+		return 0;	// temporary
+	}
+	
+	
+	// DESCRIPTION - Prompts user to enter 12 ints, stores in array
+	// PARAMETERS - int[] a
+	// RETURN TYPE - void
+	public static void enterTwelveIntegers(int[] a)  {
+		
+
+	}
+	
+	
+	// DESCRIPTION - Lists all integers in array, displays sum
+	// PARAMETERS - int[] a
+	// RETURN TYPE - void
+	public static void listAllIntegersAndSum(int[] a)  {
+		
+	
+	}
+	
+	
+	// DESCRIPTION - Lists even integers in array, displays sum
+	// PARAMETERS - int[] a
+	// RETURN TYPE - void
+	public static void listEvenIntegersAndSum(int[] a)  {
+		
+		
+	}
+	
+	
+	// DESCRIPTION - Lists odd integers in array, displays sum
+	// PARAMETERS - int[] a
+	// RETURN TYPE - void
+	public static void listOddIntegersAndSum(int[] a)  {
+		
+		
+	}
+	
+	
+}
