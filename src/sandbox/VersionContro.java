5f8839429695214c140d17f740b4e272aa52a84e
Mr. Wong <77798339+LW-OCSB@users.noreply.github.com>
2025-02-20 12:48:53 -0500

    Update VersionContro.java

--- src/sandbox/VersionContro.java
+++ src/sandbox/VersionContro.java
@@ -5,7 +5,7 @@ public class VersionContro {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World!");
-		System.out.println("FUBAR");
+		System.out.println("Foo Bar");
 	}
 
 }
