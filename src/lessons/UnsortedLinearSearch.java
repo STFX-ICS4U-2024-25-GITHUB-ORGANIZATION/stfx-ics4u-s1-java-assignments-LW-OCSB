44449f1742fb81c9a53428c49b1f5527fec00b15
e28166 <e28166@FXH-0015840124>
2025-04-08 13:55:25 -0400

    s

--- src/lessons/UnsortedLinearSearch.java
+++ src/lessons/UnsortedLinearSearch.java
@@ -3,8 +3,22 @@ package lessons;
 public class UnsortedLinearSearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+	}
+
+	// DESCRIPTION - Returns true if search pattern is found at least once inside array.
+	// PARAMETERS - String[] array, String pattern
+	// RETURN TYPE - boolean
+	public static boolean unSortedLinearSearch(String[] array, String pattern)  {
+		
+		for(int i=0; i < array.length; i++)  {
+			
+			if (array[i].equals(pattern))
+				return true;
+		}
+		
+		return false;
 	}
 
 }
