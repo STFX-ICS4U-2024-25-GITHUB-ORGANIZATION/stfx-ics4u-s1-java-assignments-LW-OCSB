ada331a3685946c80e1a83f3794282cff2e7ca8c
e28166 <e28166@FXH-0015840124>
2025-04-25 13:39:36 -0400

    s

--- src/lessons/BubbleSort.java
+++ src/lessons/BubbleSort.java
@@ -8,11 +8,29 @@ public class BubbleSort {
 
 		int[] myArray = {8, 6, 1, 5, 7, 0, 9, 3, 4, 2};
 		
+		// before sort
 		for(int i=0; i<myArray.length - 1; i++)  {
 			
 			System.out.print(myArray[i] + " ");
 		}
 
+		bubbleSort(myArray);
+		
+		// after sort
+		for(int i=0; i<myArray.length - 1; i++)  {
+			
+			System.out.print(myArray[i] + " ");
+		}
+		
 	}
 
+	
+	// DESCRIPTION - Sorts int[] in ascending order.
+	// PARAMETERS - int[]
+	// RETURN - void
+	public static void bubbleSort(int[] a)  {
+		
+		
+	}
+	
 }
