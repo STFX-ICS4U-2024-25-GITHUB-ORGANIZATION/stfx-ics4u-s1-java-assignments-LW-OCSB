ac8823b85b8b5f23b6bf6afdcd2f52876fa07b9d
e28166 <e28166@FXH-0015840124>
2025-05-06 14:13:17 -0400

    s

--- src/lessons/SelectionSort.java
+++ src/lessons/SelectionSort.java
@@ -6,13 +6,33 @@ public class SelectionSort {
 
 		int[] myArray = {3, 4, 7, 0, 8, 5, 2, 9, 6, 1};
 		
-		selectionSort(myArray);
+		selectionSortRecursive(myArray, 0);
 		
 		for (int i=0; i<myArray.length; i++)
 			System.out.print(myArray[i] + " ");
 	}
 	
 
+	public static void selectionSortRecursive(int[] a, int i)  {
+		
+		// error case
+		if (i < 0)
+			return;
+		
+		// base case / stop case
+		if(i == a.length-1)
+			return;
+		
+		// do one pass
+		for (int j=i; j<a.length-1; j++)  {
+			
+			if (a[i] > a[j+1])
+				swap(a, i, j+1);
+		}
+		
+		// do it again, starting from next element
+		selectionSortRecursive(a, i+1);
+	}
 	
 	public static void selectionSort(int[] a)  {
 
